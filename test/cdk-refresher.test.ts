761c983738a9b39275af8b70c5bdf2a57594eb0c
Marc Mendoza <777marc@gmail.com>
2025-04-29 10:42:11 -0400

    Initial commit

---
+++ test/cdk-refresher.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkRefresher from '../lib/cdk-refresher-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-refresher-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkRefresher.CdkRefresherStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
