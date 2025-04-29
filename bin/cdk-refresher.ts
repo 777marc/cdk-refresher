d633304dbae93787a6df638465edecc96208ba95
Marc Mendoza <777marc@gmail.com>
2025-04-29 18:44:15 -0400

    misc

--- bin/cdk-refresher.ts
+++ bin/cdk-refresher.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { CdkRefresherStack } from "../lib/cdk-refresher-stack";
+import { PhotoStack } from "../lib/photoStack/PhotoStack";
 
 const app = new cdk.App();
-new CdkRefresherStack(app, "CdkRefresherStack");
+//new CdkRefresherStack(app, "CdkRefresherStack");
+
+new PhotoStack(app, "PhotoBucketStack");
