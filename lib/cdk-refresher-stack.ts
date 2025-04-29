9434e014e93f21e90084f19f21af9ce758133567
Marc Mendoza <777marc@gmail.com>
2025-04-29 13:27:34 -0400

    adding params

--- lib/cdk-refresher-stack.ts
+++ lib/cdk-refresher-stack.ts
@@ -7,15 +7,22 @@ export class CdkRefresherStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const duration = new cdk.CfnParameter(this, "duration", {
+      default: 2,
+      minValue: 1,
+      maxValue: 7,
+      type: "Number",
+    });
+
     const myL2Bucket = new Bucket(this, "MyL2Bucket", {
       lifecycleRules: [
         {
-          expiration: cdk.Duration.days(2),
+          expiration: cdk.Duration.days(duration.valueAsNumber),
         },
       ],
     });
 
-    new L3Bucket(this, "MyL3Bucket", 2);
+    new L3Bucket(this, "MyL3Bucket", duration.valueAsNumber);
 
     new cdk.CfnOutput(this, "MyL3BucketName", {
       value: myL2Bucket.bucketName,
