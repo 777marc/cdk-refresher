b8a670bb2312612348059cb5106226c90c4354b0
Marc Mendoza <777marc@gmail.com>
2025-04-30 14:05:29 -0400

    final

--- bin/cdk-refresher.ts
+++ bin/cdk-refresher.ts
@@ -3,6 +3,7 @@ import * as cdk from "aws-cdk-lib";
 //import { CdkRefresherStack } from "../lib/cdk-refresher-stack";
 import { PhotoStack } from "../lib/photoStack/PhotoStack";
 import { PhotosHandlerStack } from "../lib/photoStack/PhotoHandlerStack";
+import { BucketTagger } from "../bin/Tagger";
 
 const app = new cdk.App();
 //new CdkRefresherStack(app, "CdkRefresherStack");
@@ -10,3 +11,6 @@ const photoStack = new PhotoStack(app, "PhotoStack");
 new PhotosHandlerStack(app, "PhotoHandlerStack", {
   targetBucketArn: photoStack.photosBucketArn,
 });
+
+const tagger = new BucketTagger("level", "test");
+cdk.Aspects.of(app).add(tagger);
